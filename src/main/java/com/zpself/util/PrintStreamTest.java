e001ccc1deb23a43f2ccb473732baa6d5d15f2fe
zengp <1769411197@qq.com>
2019-04-08 15:22:47 +0800

    提交

--- src/main/java/com/zpself/util/PrintStreamTest.java
+++ src/main/java/com/zpself/util/PrintStreamTest.java
@@ -1,4 +1,16 @@
 package com.zpself.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
 public class PrintStreamTest {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:/Users/ying/zpFile/project/test/test/testPrintStream.txt");
+        if(!file.getParentFile().exists()){
+            file.getParentFile().mkdirs();
+        }
+        PrintStream ps = new PrintStream(file);
+        ps.print("我在中国  我找不到人生 的意义 了吗？   迷茫    或许不是一种状态");
+    }
 }
--- src/main/java/com/zpself/util/SystemIO.java
+++ src/main/java/com/zpself/util/SystemIO.java
@@ -1,4 +1,12 @@
 package com.zpself.util;
 
+import java.util.Scanner;
+
 public class SystemIO {
+    public static void main(String[] args) {
+        System.out.println("请输入----");
+        Scanner sn = new Scanner(System.in);
+        int a=sn.nextInt();
+        System.out.println("输入结束----"+a);
+    }
 }
